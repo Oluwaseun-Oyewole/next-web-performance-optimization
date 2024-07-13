4ede1875cb7bdbab3f2675c6ec79794da19854ad
Oluwaseun Oyewole <findseunoyewole@gmail.com>
2024-07-13 10:48:21 +0100

    complete notes on generics

--- app/typescripts/generics/page.tsx
+++ app/typescripts/generics/page.tsx
@@ -1,5 +1,47 @@
 const TypescriptGenerics = () => {
-  return <div>TypescriptGenerics</div>;
+  return (
+    <div>
+      <h1 className="pb-2 text-2xl text-blue-500 font-bold">
+        Typescript Generics
+      </h1>
+      <p className="font-[300] text-sm">
+        TG allows us to create reusable functions that can work with a variety
+        of types while also maintaining full type safety
+      </p>
+      <section>
+        <h2>Generic Functions</h2>
+        <code className="text-sm">
+          function identity{`<T>(value:T):T{return value}`}
+        </code>
+      </section>
+
+      <section className="py-2">
+        <h2>Generic Interfaces</h2>
+        <code className="text-sm">
+          interface KeyPairs{`<T, P>{key:T, value:P}`}
+        </code>
+        <br />
+        <code className="text-sm">
+          const numberPair: KeyValuePair
+          {`<number, string> = { key: 1, value: "one" }`}
+        </code>
+      </section>
+      <section className="text-sm">
+        <h2 className="text-lg pb-2">Other generics use cases</h2>
+        <code>
+          function mapPair
+          {`<K, V>(key: K, value: V): [K, V] {
+  return [key, value];`}
+        </code>
+        <br />
+        <code>
+          function createArray
+          {`<T = string>(length: number, value: T): T[] {
+  return new Array(length).fill(value);`}
+        </code>
+      </section>
+    </div>
+  );
 };
 
 export default TypescriptGenerics;
